3df3cc5c9efc3931ad24e4d565e56937c2678788
motochowdhury <motochowdhury@gmail.com>
2023-01-05 20:34:57 +0600

    Git setup

---
+++ index.js
@@ -0,0 +1,9 @@
+// const lowerCase = paragraph.toLocaleLowerCase();
+// const replaced_text = lowerCase.replace("chowdury", "chowdhury");
+
+function customizeText() {
+  const textAreText = document.getElementsByClassName(".textAreaCont");
+
+  console.log(textAreText);
+  //   console.log("moto");
+}
